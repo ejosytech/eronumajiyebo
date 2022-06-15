bf7f9f611a62fb50c829ad2c947ada3834880204
ejosytech <eronu@ejosytech.com>
2022-06-15 13:32:47 +0100

    commit-june-15-2022

--- wp-content/plugins/site-content/templates/cv.php
+++ wp-content/plugins/site-content/templates/cv.php
@@ -2,9 +2,11 @@
     <main class="page cv-page">
         <section class="portfolio-block block-intro border-bottom">
             <div class="container">
-                <div class="avatar" style="background-image:url('<?php bloginfo("template_directory");?>/img/avatars/me.jpg');"></div>
+                <div class="avatar" style="background-image:url('<?php echo plugin_dir_url(__Dir__);?>/img/me-suite.jpg');"></div>
                 <div class="about-me">
-                    <p>Hello! I am <strong>John Smith</strong>. I work as interface and front end developer. I have passion for pixel perfect, minimal and easy to use interfaces.</p><a class="btn btn-outline-primary" role="button" href="<?php echo site_url(); ?>/member-login/"">More About Me</a></div>
+                    <p><strong>Dr. Emmanuel M. Eronu</strong> </p>
+                    <p>I am Self-motivated, well organized, reliable and competent with extensive skills and experience in developing conceptualized solutions. I am able to remain calm under pressure and retain a sense of humour while adapting to current trends of new technologies. Over the years, I have consistently strived to create a challenging learning environment where students can become lifelong scholars and learners. One of my cardinal objectives is to contribute significantly to achieving organizational objectives by adding value to all operational activities via ICT technologies. </p>
+                </div>
             </div>
         </section>
         <section class="portfolio-block cv">
@@ -14,35 +16,111 @@
                         <h2 class="text-center">Work Experience</h2>
                     </div>
                     <div class="item">
+                        <h3>Lead Software Engineer/Consultant </h3>
                         <div class="row">
-                            <div class="col-md-6">
-                                <h3>Web Developer</h3>
-                                <h4 class="organization">Amazing Co.</h4>
-                            </div>
-                            <div class="col-md-6"><span class="period">10/2013 - 04/2015</span></div>
+                            <div class="col-md-6"><a href="https://www.ejosytech.com"><h4 class="organization">Ejosy Tech Consult Ltd </h4></a></div>
+                            <div class="col-md-6"><span class="period">2008 - To Date</span></div>
                         </div>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget velit ultricies, feugiat est sed, efficitur nunc, vivamus vel accumsan dui.</p>
+                         <div class="row">
+                            <div class="col-md-6"><a href="#"> <h4 class="organization">Intelligize Allied-Agro Ltd</h4></a></div>
+                            <div class="col-md-6"><span class="period">2016 - To Date</span></div>
+                        </div>
+                        <div class="row">
+                            
+                                 <div class="card" >
+                                    <div class="card-body">
+                                        <h5 class="card-title">Role | Responsibility</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Speciality</h6>
+                                        <ul>
+                                            <li>Embedded System Design, Development and Deployment</li>
+                                            <li>IoT Application Design, Development and Deployment</li>
+                                            <li>Full Stack Software Developer</li>
+                                            <li>Digital Education Expert</li>
+                                        </ul>
+                                        <h6 class="card-subtitle mb-2 text-muted">Management</h6>
+                                        <ul>
+                                            <li>.....</li>
+                                            <li>.........</li>
+                                        </ul>
+                                            <a href="#" class="card-link">Card link</a>
+                                            <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                     <div class="card-body">
+                                        <h5 class="card-title">Selected Accomplishments</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Services</h6>
+                                        <ul>
+                                            <li>....</li>
+                                            <li>.....</li>
+                                        </ul>
+                                        <h6 class="card-subtitle mb-2 text-muted">Products</h6>
+                                        <ul>
+                                            <li>.....</li>
+                                            <li>........</li>
+                                        </ul>
+                                            <a href="https://github.com/ejosytech" class="card-link">Github</a>
+                                            <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                </div>
+                         
+                        </div>
+                    
                     </div>
+                    
                     <div class="item">
+                        <h3>Senior Lecturer/Researcher </h3>
                         <div class="row">
-                            <div class="col-6">
-                                <h3>Front End Developer</h3>
-                                <h4 class="organization">Innovative Org.</h4>
-                            </div>
-                            <div class="col-md-6"><span class="period">05/2015 - 12/2017</span></div>
+                            <div class="col-md-6"><a href="https://www.uniabuja.edu.ng"><h4 class="organization">University of Abuja </h4></a></div>
+                            <div class="col-md-6"><span class="period">2008 - To Date</span></div>
                         </div>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget velit ultricies, feugiat est sed, efficitur nunc, vivamus vel accumsan dui.</p>
-                    </div>
-                    <div class="item">
+                         <div class="row">
+                             <div class="col-md-6"><a href="https://futminna.edu.ng/"> <h4 class="organization">Federal University of Technology, Minna</h4></a></div>
+                            <div class="col-md-6"><span class="period">2016 - To Date</span></div>
+                        </div>
                         <div class="row">
-                            <div class="col-md-6">
-                                <h3>Web Developer</h3>
-                                <h4 class="organization">Special Inc.</h4>
-                            </div>
-                            <div class="col-md-6"><span class="period">12/2017 - Present</span></div>
+                            
+                                 <div class="card" >
+                                    <div class="card-body">
+                                        <h5 class="card-title">Academia | Research </h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Research Interest</h6>
+                                        <ul>
+                                            <li>Internet of Things</li>
+                                            <li>Wireless Communication[4G & 5G]</li>
+                                            <li>Spectrum Management</li>
+                                            <li>Wireless Sensor Networks</li>
+                                            <li>Digital Education</li>
+                                        </ul>
+                                        <h6 class="card-subtitle mb-2 text-muted">Courses Taught(Selected)</h6>
+                                        <ul>
+                                            <li>Advance Wireless Networks and Protocols</li>
+                                            <li>Electromagnetic Field and Waves I & II</li>
+                                            <li>Microprocessor and Microcomputer Systems</li>
+                                            <li>Lowlevel and Highlevel  Programming Languages</li>
+                                            <li>Digital Signal Processing</li>
+                                            <li>Computer Programming</li>
+                                            <li>System Integration</li>
+                                        </ul>
+                                           
+                                    </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Administration | Management (Selected)</h5>
+                                         <ul>
+                                            <li><strong>Coordinator,</strong>Computer Engineering Dept</li>
+                                            <li><strong>Head,</strong>Embedded System & IoT Research Group</li>
+                                            <li><strong>Director,</strong>Student Industrial Works Experience Scheme</li>
+                                            <li><strong>Chairman,</strong>Faculty of Engineering's Committee on ICT</li><!-- comment -->
+                                        </ul>
+                                     </div>
+                                     
+                                        <a href="https://lms.eronumajiyebo.online" class="card-link">Online learning Platform</a>
+                                        <a href="https://embiot.eronumajiyebo.online" class="card-link">Research group portal</a>
+                                     
+                                </div>
+                         
                         </div>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget velit ultricies, feugiat est sed, efficitur nunc, vivamus vel accumsan dui.</p>
+                    
                     </div>
+                    
+                    
                 </div>
                 <div class="education group">
                     <div class="heading">
@@ -80,7 +158,7 @@
                                 </div>
                                 <h3>PHP</h3>
                                 <div class="progress">
-                                    <div class="progress-bar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"><span class="sr-only">90%</span></div>
+                                    <div class="progress-bar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"><span class="sr-only">90%</span></div>
                                 </div>
                                 <h3>JavaScript</h3>
                                 <div class="progress">
@@ -97,15 +175,15 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-1"><i class="icon ion-person icon"></i></div>
-                                    <div class="col-9"><span>John Smith</span></div>
+                                    <div class="col-9"><span>Dr. Emmanuel Eronu</span></div>
                                 </div>
                                 <div class="row">
                                     <div class="col-1"><i class="icon ion-ios-telephone icon"></i></div>
-                                    <div class="col-9"><span>+235 3217 424</span></div>
+                                    <div class="col-9"><span>+234 8033927733</span></div>
                                 </div>
                                 <div class="row">
                                     <div class="col-1"><i class="icon ion-at icon"></i></div>
-                                    <div class="col-9"><span>lorem@email.com</span></div>
+                                    <div class="col-9"><span>mail@eronumajiyebo.online</span></div>
                                 </div>
                             </div>
                         </div>
