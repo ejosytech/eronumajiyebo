76bb6d3cc1f58a4686ee584d47b28a141c18ad17
ejosytech <eronu@ejosytech.com>
2022-06-18 13:34:53 +0100

    commit-june-18-2022

--- wp-content/plugins/site-content/templates/accomplish.php
+++ wp-content/plugins/site-content/templates/accomplish.php
@@ -1,32 +1,96 @@
 <?php  get_header('home'); ?> 
-<main class="page project-page">
-       <section class="portfolio-block project">
-              <div class= "container">
-                  
-                <div class ="row ">
-                    <div class ="col-4"></div>
-                    <div class ="col-4">
-                    <img src="<?php bloginfo("template_directory");?>\uploads\images\under_constructn\under_constr.jpeg" alt="Site Under Construction">
-                    </div>
-                     <div class ="col-4"></div>
-                </div>
-              </div>
-            </section>
-      </main>
- <!-- 
 
-    <main class="page project-page">
-     <section>
-            <div class="container">
-                <div class="heading">
-                    <h2>Accomplishment</h2>
-                    <h3>Projects</h3>
-                    <h3>Publications</h3>
+<main class="page cv-page">
+<section class="portfolio-block block-intro border-bottom">
+     <div class="container">
+     <div class="row">
+          <div class="heading">
+                        <h2 class="text-center">Accomplishments</h2>
+                    </div>
+                        
+                            <div class="skills portfolio-info-card">
+                               
+                                <!-- Corousal - Begin-------->
+                                
+                                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-indicators">
+                                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                        </div>
+                                        <div class="carousel-inner">
+                                          <div class="carousel-item active">
+                                            <img src="<?php echo plugin_dir_url(__Dir__);?>/img/accomplish/tsuga-open-6x3.jpg" class="d-block w-100" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                              <h5>Tianjin Sino-German University of Applied Sciences (TSGUAS), Tianjin, China</h5>
+                                              <p>Award of Certificate for the Completion of Technical Training in Cutting Edge Technologies (5G and IoT)</p>
+                                            </div>
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img src="<?php echo plugin_dir_url(__Dir__);?>/img/accomplish/Tsuga-cert-6x3.jpg" class="d-block w-100" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                              <h5>Tianjin Sino-German University of Applied Sciences (TSGUAS), Tianjin, China</h5>
+                                              <p>Award of Certificate for the Completion of Technical Training in Cutting Edge Technologies (5G and IoT)</p>
+                                            </div>
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img src="<?php echo plugin_dir_url(__Dir__);?>/img/accomplish/epfl-6x3.jpg" class="d-block w-100" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                              <h5>World bank-sponsored training of Digital Education Expert held in Kenya.</h5>
+                                              <p>Facilitated by École Polytechnique fédérale de Lausanne, Switzerland.</p>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Next</span>
+                                        </button>
+                                      </div>
+                                <!------------------------Corosel End----------------->
+                                </div>
+                            </div>
+         </div>
+</section>
+<section class="portfolio-block block-intro border-bottom">
+    <div class="container">
+<div class="group">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="skills portfolio-info-card">
+                                <h2>Academia | Research</h2>
+                                <div class="card" >
+                                    <div class="card-body">
+                                        <p align="left"> Digital Education Expert <a href="https://lms.eronumajiyebo.online">[lms.eronumajiyebo.online]</a> </p>
+                                        <p align="left">Administered and Managed successfully the SIWES Directorate and Computer Engineering Dept </p> 
+                                        <p align="left">Mentored and Coordinated Research Work in Embedded System and IoT </p>
+                                        <p align="left">Publications</p> 
+                                                                            
+                                    </div><!-- </div> -->
+                                    </div>
+                                
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="skills portfolio-info-card">
+                            <div class="contact-info portfolio-info-card">
+                                <h2> Software Engineering | Consulting</h2>
+                                <div class="card" >
+                                    <div class="card-body">
+                                 <p align="left">Projects </p>
+                                  <p align="left">Product</p>
+                                  <p align="left">Speciality</p>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                
-            </div>
-        </section>
+        </div>
+                        </div>
+    </div>
+</section>
     </main>
- -->
-    
  <?php get_footer();?> 
